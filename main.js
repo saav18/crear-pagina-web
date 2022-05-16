35ce296ede10f7bafabff6af34e2b26170ff57a4
saav18 <105613081+saav18@users.noreply.github.com>
2022-05-15 23:50:08 -0300

    Update main.js

--- main.js
+++ main.js
@@ -6,8 +6,3 @@ ScrollReveal().reveal('.showcase');
 ScrollReveal().reveal('.news-cards', { delay: 500 });
 ScrollReveal().reveal('.cards-banner-one', { delay: 500 });
 ScrollReveal().reveal('.cards-banner-two', { delay: 500 });
-Terms
-Privacy
-Security
-Status
-Docs
